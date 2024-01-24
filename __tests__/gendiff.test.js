c188c42cc6b4e5ae7c1a8024d0b7b297e81ddd09
gleeebkaaa <g.pudovkin1996@gmail.com>
2024-01-24 17:03:21 +0300

    touch fixtures/add filepath in tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,14 @@
 import generateDiff from '../src/index.js';
+import path from 'path';
+import fs from 'fs';
+
+const getFixturePath = (filename) => path.join('__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('generateDiff', () => {
     test('добавление нового ключа', () => {
-        const data1 = { a: 1, b: 2 };
-        const data2 = { a: 1, b: 2, c: 3 };
+        const data1 = readFile('before_add.json');
+        const data2 = readFile('after_add.json');
         const expected = `{
             a: 1
             b: 2
@@ -13,8 +18,8 @@ describe('generateDiff', () => {
     });
 
     test('удаление существовавшего ключа', () => {
-        const data1 = { a: 1, b: 2, c: 3 };
-        const data2 = { a: 1, b: 2 };
+        const data1 = readFile('before_remove.json');
+        const data2 = readFile('after_remove.json');
         const expected = `{
             a: 1
             b: 2
@@ -24,8 +29,8 @@ describe('generateDiff', () => {
     });
 
     test('изменение значения существующего ключа', () => {
-        const data1 = { a: 1, b: 2 };
-        const data2 = { a: 2, b: 2 };
+        const data1 = readFile('before_change.json');
+        const data2 = readFile('after_change.json');
         const expected = `{
           - a: 1
           + a: 2
@@ -35,21 +40,21 @@ describe('generateDiff', () => {
     });
 
     test('изменение значения ключей и их порядка', () => {
-        const data1 = { a: 1, b: 2, c: 4 };
-        const data2 = { a: 2, b: 2, d: 3 };
+        const data1 = readFile('before_change_order.json');
+        const data2 = readFile('after_change_order.json');
         const expected = `{
           - a: 1
           + a: 2
             b: 2
           + d: 3
-          - c: 4  
+          - c: 4
         }`;
         expect(generateDiff(data1, data2)).toBe(expected);
     });
 
     test('без изменений', () => {
-        const data1 = { f: 1, g: 2 };
-        const data2 = { f: 1, g: 2 };
+        const data1 = readFile('before_no_change.json');
+        const data2 = readFile('after_no_change.json');
         const expected = `{
             f: 1
             g: 2
