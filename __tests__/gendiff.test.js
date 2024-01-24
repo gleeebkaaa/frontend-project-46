dbcc24cf9045a3a756c9672e2d06ea37274c18fb
gleeebkaaa <g.pudovkin1996@gmail.com>
2024-01-24 17:14:42 +0300

    modified getFixturesPath

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,6 +1,8 @@
-import generateDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
 import path from 'path';
-import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
